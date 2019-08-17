98c119c310bda5ac45408a002adb61e5ce205eb2
Andrew Mushel <almushel@gmail.com>
2019-08-17 15:50:48 -0500

    aimerX limited by distance from active character

--- js/Aimer.js
+++ js/Aimer.js
@@ -1,7 +1,8 @@
 const DISTANCE_PER_ACTION = 200;
 
 var aimerX = 0,
-    aimerY = 0;
+    aimerY = 0,
+    outOfRangeY = 0;
 
 function moveAimer() {
     aimerX = mousePos.x;
@@ -9,17 +10,45 @@ function moveAimer() {
     if (isAiming) {
         return;
     }
-    
-    checkCol = colAtXCoord(aimerX),
-    startRow = rowAtYCoord(aimerY),
-    checkIndex = brickTileToIndex(checkCol, startRow);
+
+    //If character1 is active set to character 1, else set to character 2
+    let activeChar = character1.isActive ? character1 : character2,
+    xDelta = activeChar.characterX - aimerX;
+    if (Math.abs(xDelta) > DISTANCE_PER_ACTION) {
+        outOfRangeY = findGround(mousePos.x, mousePos.y);
+         //Set aimerX to max distance in the correct direction
+        aimerX = xDelta > 0 ? activeChar.characterX - DISTANCE_PER_ACTION : activeChar.characterX + DISTANCE_PER_ACTION;
+    }
+
+    aimerY = findGround(aimerX, aimerY);
+}
+
+function drawAimer() {
+    if (isAiming) {
+        canvasContext.drawImage(targetAimerPic, aimerX - targetAimerPic.width / 2, aimerY - targetAimerPic.height / 2);
+    } else {
+        if (aimerX != mousePos.x) {
+            colorRect(mousePos.x - moveAimerPic.width / 4, outOfRangeY - targetAimerPic.height, moveAimerPic.width/2, moveAimerPic.height, 'red')
+            canvasContext.drawImage(moveAimerPic, mousePos.x - moveAimerPic.width / 2, outOfRangeY - targetAimerPic.height);
+        }
+
+        canvasContext.drawImage(moveAimerPic, aimerX - moveAimerPic.width / 2, aimerY - targetAimerPic.height);
+    }  
+}
+
+function findGround(x, y) {
+    let groundY = y;
+        checkCol = colAtXCoord(x),
+        startRow = rowAtYCoord(y),
+        checkIndex = brickTileToIndex(checkCol, startRow);
+
     //If the cursor is already inside of a wall
     if (brickGrid[checkIndex] > 0) {
         //Find nearest empty tile above the cursor
         for (let i = startRow; i >= 0; i--) {
             checkIndex = brickTileToIndex(checkCol, i);
             if (brickGrid[checkIndex] < 1) {
-                aimerY = (i + 1) * BRICK_H;
+                groundY = (i + 1) * BRICK_H;
                 break;
             }
         }
@@ -29,17 +58,11 @@ function moveAimer() {
         for (let i = startRow; i < BRICK_ROWS; i++) {
             checkIndex = brickTileToIndex(checkCol, i);
             if (brickGrid[checkIndex] > 0) {
-                aimerY = i * BRICK_H;
+                groundY = i * BRICK_H;
                 break;
             }
         }
     }
-}
 
-function drawAimer() {
-    if (isAiming) {
-        canvasContext.drawImage(targetAimerPic, aimerX - targetAimerPic.width / 2, aimerY - targetAimerPic.height / 2);
-    } else {
-        canvasContext.drawImage(moveAimerPic, aimerX - moveAimerPic.width / 2, aimerY - targetAimerPic.height);
-    }  
+    return groundY;
 }
