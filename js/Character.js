64e481ffcca5c94839c14918c5d73cc447a907f3
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-10-20 12:48:14 -0400

    Removed unused sprite functions from Character Class

--- js/Character.js
+++ js/Character.js
@@ -63,57 +63,10 @@ function characterClass(character_team, character_color) {
     x: 0,
     y: 0
   };
-  
-  //Test Items from Skeletal Animation System START
-  
-  //Sprites Array
-this.newCharacterhead = document.createElement("img");
-this.newCharacterheadLoaded = false;
-this.newCharacterUpperArm = document.createElement("img");
-this.newCharacterUpperArmLoaded = false;
-this.newCharacterForeArm = document.createElement("img");
-this.newCharacterForeArmLoaded = false;
-this.newCharacterTorso = document.createElement("img");
-this.newCharacterTorsoLoaded = false;
-
-
-this.loadCharacterImages = function(){	
-
-	this.newCharacterhead.onload = function(){
-		newCharacterheadLoaded = true;
-	}
-	this.newCharacterhead.src = "images/cowboyHatRight.png"; //image file path here
-	
-	this.newCharacterTorso.onload = function(){
-		newCharacterTorsoLoaded = true;
-	}
-	this.newCharacterTorso.src = "images/characterBodyPlaceHolderRight.png"; //image file path here
-	
-	this.newCharacterUpperArm.onload = function(){
-		newCharacterUpperArmLoaded = true;
-	}
-	this.newCharacterUpperArm.src = "images/characterUpperArmPlaceHolder.png"; //image file path here	
-	
-	this.newCharacterForeArm.onload = function(){
-		newCharacterForeArmLoaded = true;
-	}
-	this.newCharacterForeArm.src = "images/characterLowerArmPlaceHolder.png"; //image file path here
-}
-  //Postions and angles arrays
-
-  
-	this.positionsX = new Array ([389.5,-12.334998092074102,-39.19778369209928,2],[389.5,-17.6664036114567,-40.12229966823247,2],[389.5,-19.242158903144627,-42.61366139095975,3.4217289612493005],[389.5,-17.777514585095105,-45.412278334393875,1.946605563810408]);
-	this.positionsY = new Array([410,5.1304433649737575,9.941610039203908,46.5],[410,12.287998358428808,27.79571602568791,46.5],[410,24.607331983155632,49.33138782478426,57.06028235632431],[410,12.536700913860727,25.672088494881393,44]);
-	this.limbAngles = new Array([0,0.8537845069879517,-1.6652155997563742,-0.04270905576500415],[0,0.4266274931268761,-1.6652155997563742,-0.04270905576500415],[0,-0.17219081452293902,-1.0225510836340417,-0.22494126505117096],[0,0.4136892132788633,-1.3851392906188422,0]);
-  	
- //Test Items from Skeletal Animation System END
 
   this.drawCharacter = function () {
-	//this.setCharacterSprites();
-    this.shoulderOffset = -this.torsoSprite.height / 4;
 
-    
-	  //this.loadCharacterImages();
+    this.shoulderOffset = -this.torsoSprite.height / 4;   	
 
     //Here's another method that might look better than the tinted sprite approach.
     //To test this, uncomment this and the last line, and set the inActiveColor and usedColor to #00000000 in the Assets.js.
@@ -134,8 +87,8 @@ this.loadCharacterImages = function(){
 		  this.drawHat(this.x, this.y - this.torsoSprite.height / 3, 1.9 * Math.PI);
 		}
 	}else{
-		//idle animation
-		//this.drawFromAnimationData();
+		//TODO: idle animation here
+		//drawFromAnimationData(this, );
 	}
     if (isInAimMode && this.isActive){
       this.drawWeapon(this.rightHand.x + GUN_POS, this.rightHand.y);
@@ -260,15 +213,6 @@ this.loadCharacterImages = function(){
       this.x = (1 + Math.floor(this.x / BRICK_W)) * BRICK_W - (this.torsoSprite.width / 2);
     }
 
-  }
-  
-  //Animation test
-  this.drawFromAnimationData = function(){
-	  for(i=1; i < this.spriteArray.length; i++){
-			//Sprites drawn relavtive to character center
-			drawImageCenteredAtLocationWithRotation(this.spriteArray[i], ((this.x - (this.torsoSprite.width / 2)) - (this.positionsX[animationIndex] [i])), (this.y - (this.torsoSprite.height / 2)) - (this.positionsY[animationIndex] [i]), this.limbAngles[animationIndex] [i])
-			
-		}
   }
 
   this.drawBody = function () {
