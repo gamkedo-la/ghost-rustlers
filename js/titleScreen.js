a13bd75ea534ff160b2991f3a869413fcc10ddc5
Gamkedo <chris@gamkedo.com>
2019-10-31 23:59:47 -0500

    Title menu button refactor (loops over an object array) before adding credits button

--- js/titleScreen.js
+++ js/titleScreen.js
@@ -1,3 +1,16 @@
+var buttonList = [
+    {text:"Level 1", action: function() { startLevel(allLevels[0]); } },
+    {text:"Level 2", action: function() { startLevel(allLevels[1]); } },
+    {text:"Level 3", action: function() { startLevel(allLevels[2]); } },
+    {text:"Random", action: function() { startLevel(generateRandomLevel()); } }
+];
+
+function startLevel(whichLevel) {
+    levelTileGrid = whichLevel; // which level data to use?
+    updateState(STATE_GAME); // start playing!
+    hauntedHoedownSound.loopSong("hauntedHoedown"); // start music
+}
+
 function drawTitleScreen(){
     background.draw(Math.sin(performance.now()/5000)*100-150,0);
 	colorText("GHOST RUSTLERS", (canvas.width/2-300), (canvas.height/2-100), 'white', "64px Arial Black");
@@ -5,18 +18,10 @@ function drawTitleScreen(){
     var bx = canvas.width/2-25;
     var by = canvas.height/2+5;
 
-    canvasContext.drawImage(buttonPic,(canvas.width/2-50), (canvas.height/2-25), 100, 50);
-	colorText("Level 1", bx, by, 'white');
-
-    canvasContext.drawImage(buttonPic,(canvas.width/2-50), (canvas.height/2-25) + 60, 100, 50);
-	colorText("Level 2", bx, by + 60, 'white');
-
-    canvasContext.drawImage(buttonPic,(canvas.width/2-50), (canvas.height/2-25) + 60 + 60, 100, 50);
-	colorText("Level 3", bx, by + 60 + 60, 'white');
-
-    canvasContext.drawImage(buttonPic,(canvas.width/2-50), (canvas.height/2-25) + 60 + 60 + 60, 100, 50);
-	colorText("Random", bx-5, by + 60 + 60 + 60, 'white');
-
+    for(var i=0;i<buttonList.length;i++) {
+        canvasContext.drawImage(buttonPic,(canvas.width/2-50), (canvas.height/2-25)+60*i, 100, 50);
+        colorText(buttonList[i].text, bx-4, by+60*i, 'white');
+    }
 }
 
 function titleScreenMouseClick(mousePosX, mousePosY){
@@ -26,48 +31,13 @@ function titleScreenMouseClick(mousePosX, mousePosY){
     var x = canvas.width/2-50;
     var y = canvas.height/2-25;
 
-    // level 1 clicked
-	if(	(mousePosX > x) && // left side
-		(mousePosX < x + w) && //right side
-		(mousePosY > y) && //top side
-		(mousePosY < y + h)) //bottom side
-	{		
-        levelTileGrid = allLevels[0]; // which level data to use?
-        updateState(STATE_GAME); // start playing!
-		hauntedHoedownSound.loopSong("hauntedHoedown"); // start music
-	}
-
-    // level 2 clicked
-	if(	(mousePosX > x) && // left side
-		(mousePosX < x + w) && //right side
-		(mousePosY > y + 60) && //top side
-		(mousePosY < y + 60 + h)) //bottom side
-	{		
-        levelTileGrid = allLevels[1]; // which level data to use?
-        updateState(STATE_GAME); // start playing!
-		hauntedHoedownSound.loopSong("hauntedHoedown"); // start music
-	}
-
-    // level 3 clicked
-	if(	(mousePosX > x) && // left side
-		(mousePosX < x + w) && //right side
-		(mousePosY > y + 60 + 60) && //top side
-		(mousePosY < y + 60 + 60 + h)) //bottom side
-	{		
-        levelTileGrid = allLevels[2]; // which level data to use?
-        updateState(STATE_GAME); // start playing!
-		hauntedHoedownSound.loopSong("hauntedHoedown"); // start music
+    for(var i=0;i<buttonList.length;i++) {
+        if( (mousePosX > x) && // left side
+            (mousePosX < x + w) && //right side
+            (mousePosY > y + 60*i) && //top side
+            (mousePosY < y + 60*i + h)) //bottom side
+        {
+            buttonList[i].action();       
+        }
     }
-    
-    // level 4 clicked
-	if(	(mousePosX > x) && // left side
-    (mousePosX < x + w) && //right side
-    (mousePosY > y + 60 + 60 + 60) && //top side
-    (mousePosY < y + 60 + 60 + 60 + h)) //bottom side
-{		
-    levelTileGrid = generateRandomLevel(); // woo hoo!
-    updateState(STATE_GAME); // start playing!
-    hauntedHoedownSound.loopSong("hauntedHoedown"); // start music
-}
-
 }
